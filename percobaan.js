276f44e2026ea078420fb50fc87315ae2c178425
ZufarIH <zufar.zih@gmail.com>
2024-09-30 19:38:47 +0700

    nambah komen4

--- percobaan.js
+++ percobaan.js
@@ -26,6 +26,7 @@ http.createServer(function(request, response){
         <li>Pekerjaan      : Pengusaha </li>
     `
     )}
+    // untuk menangani URL yang tidak ada
     else{
         response.end("<h1>404: Halaman Ini Tidak Ada</h1><hr>")
     }
