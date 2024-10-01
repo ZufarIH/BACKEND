e635f7f1d588e39521c41a7b54af71f5b065c456
ZufarIH <zufar.zih@gmail.com>
2024-10-01 21:53:22 +0700

    view engine ejs & konsumsi data dari backend

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine','ejs') //setting penggunaan template engine untuk express
+app.set('views', './view-ejs') //setting penggunaan folder untuk penyimpanan file ejs
+
 // request singkat jadi req
 // response singkat jadi res
+
+// fungsi render ('nama file')
+// nama filenya wajib bereksistensi .ejs
+// otomatis mengambil file .ejs yang ada di folder view-ejs
 app.get('/', function (request, response) {
-    response.send('<h1>Hello World</h1>')
+    response.render('beranda')
 })
 app.get('/pendidikan', function (request, response) {
-    response.send('<h1>Riwayat Pendidikan</h1>')
+    let profil={
+        nama: 'Gepard Beckham',
+        S1:'ITS',
+        SMA: 'Sejahtera',
+    }
+    response.render('page-pendidikan', profil)
 })
 app.get('/karyawan', function (request, response) {
-    response.send('<h1>Data Karyawan</h1>')
+    response.render('page-karyawan')
 })
 
 
