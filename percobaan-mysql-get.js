0722712ac45bdb3141db09bc2a985f775d334041
ZufarIH <zufar.zih@gmail.com>
2024-10-01 20:06:20 +0700

    kombinasi request db & server web

--- percobaan-mysql-get.js
+++ percobaan-mysql-get.js
@@ -1,4 +1,6 @@
 const mysql= require('mysql2')
+const http=require(`http`)
+const fs=require("fs")
 
 // koneksi ke myqgl
 const db = mysql.createConnection({
@@ -18,12 +20,44 @@ db.connect((error)=>{
     }
 })
 
-db.query('SELECT * FROM karyawan2', function(errorSql, hasil){
-    if(errorSql){
-        console.log(errorSql);
-        
-    }else{
-        console.log(hasil)
-        
+// new promise memaksa data di jalankan dahulu
+// async + await di paksa ke selanjutnya
+function getAll_karyawan(){
+    return new Promise((resolve, reject)=>{
+        db.query('SELECT * FROM karyawan2', function(errorSql, hasil){
+        if(errorSql){
+             reject(errorSql) 
+        }else{
+            resolve(hasil)   
+        }
+        })
+    })
+}
+// async disini
+http.createServer(async function(request, response){
+    response.writeHead(200, {"Content-type": "text/html"})
+
+    if(request.url=="/"){
+    fs.createReadStream("./View/HalamanUtama.html").pipe(response)
     }
+    else if(request.url=="/karyawan"){
+
+        // tarik data dari db (database)
+        // await disini
+        let data=await getAll_karyawan()
+        response.end(
+            `<h1>Data penghuni gunung</h1>
+            <hr>
+            Nama Lengkap: ${data[0].nama} <br>
+            Nomor Induk Karyawan: ${data[0].NIK} <br>
+            <pre>
+            ${JSON.stringify(data,null,4)}
+            </pre>`
+            // JSON buat ngedebug aja
+        )
+
+    }
+
+}).listen(3000, function(){
+    console.log(`server sudah nyala, buka http://localhost:3000`);
 })
