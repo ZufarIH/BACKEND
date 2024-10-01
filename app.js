ad43909d2fd03225c8d9990f5cdfab1e16a11464
ZufarIH <zufar.zih@gmail.com>
2024-10-01 21:10:02 +0700

    menginstal nodemon

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+
+// request singkat jadi req
+// response singkat jadi res
+app.get('/', function (request, response) {
+    response.send('<h1>Hello World</h1>')
+})
+app.get('/pendidikan', function (request, response) {
+    response.send('<h1>Riwayat Pendidikan</h1>')
+})
+app.get('/karyawan', function (request, response) {
+    response.send('<h1>List Karyawan</h1>')
+})
+
+
+app.listen(3000, ()=>{
+    console.log(`server sudah nyala, buka http://localhost:3000`);
+    
+})
